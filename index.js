0b6d441187b7f7f404e075d00ec9469d1c1ec744
Atikarn duangdeeden <118290845+hazellza@users.noreply.github.com>
2024-05-04 16:49:16 +0700

    confix api

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const mysql = require('mysql2')
+const bcrypt = require('bcrypt')
 require('dotenv').config()
 const app = express()
 
@@ -33,26 +34,87 @@ app.get('/users/:id', (req, res) => {
     )
 })
 
-app.post('/users', (req, res) => {
+
+app.post('/register', (req, res) => {
     connection.query(
-        'INSERT INTO `users` (`fname`, `lname`, `username`, `password`, `phonenumber`, `avatar`) VALUES (?, ?, ?, ?, ?)',
-        [req.body.fname, req.body.lname, req.body.username, req.body.password, req.body.phonenumber, req.body.avatar],
-         function (err, results, fields) {
+        'SELECT * FROM `users` WHERE `username` = ?',
+        [req.body.username],
+        function (err, results, fields) {
             if (err) {
-                console.error('Error in POST /users:', err);
-                res.status(500).send('Error adding user');
+                console.error('Error in checking username:', err);
+                res.status(500).send('Error checking username');
             } else {
-                res.status(201).send(results);
+                if (results.length > 0) {
+                    res.status(400).send('Username already exists');
+                } else {
+                    // ทำการ hash หรือเข้ารหัส password ก่อนที่จะ เก็ยลง ฐานข้อมูล
+                    bcrypt.hash(req.body.password, 10, function (err, hash) {
+                        if (err) {
+                            console.error('Error hashing password:', err);
+                            res.status(500).send('Error hashing password');
+                        } else {
+                            connection.query(
+                                'INSERT INTO `users` (`fname`, `lname`, `username`, `password`, `phonenumber`, `avatar`) VALUES (?, ?, ?, ?, ?, ?)',
+                                [req.body.fname, req.body.lname, req.body.username, hash, req.body.phonenumber, req.body.avatar],
+                                function (err, results, fields) {
+                                    if (err) {
+                                        console.error('Error in POST /register:', err);
+                                        res.status(500).send('Error adding user');
+                                    } else {
+                                        res.status(201).send('Register successful');
+                                    }
+                                }
+                            );
+                        }
+                    });
+                }
             }
         }
-    )
-})
+    );
+});
+
+
+app.post('/login', function (req, res) {
+    connection.query(
+        'SELECT * FROM `users` WHERE username = ?',
+        [req.body.username],
+        function (err, results) {
+            if (err) {
+                console.error('Error in login:', err);
+                res.status(500).send('Error logging in');
+            } else {
+                if (results.length > 0) {
+                    // ดูว่า username มีใน ฐานไหม
+                    bcrypt.compare(req.body.password, results[0].password, function (err, result) {
+                        if (err) {
+                            console.error('Error comparing passwords:', err);
+                            res.status(500).send('Error logging in');
+                        } else {
+                            if (result) {
+                                // Passwords match, login successful
+                                res.status(200).send('Login successful');
+                            } else {
+                                // Passwords don't match
+                                res.status(401).send('Login failed');
+                            }
+                        }
+                    });
+                } else {
+                    // User not found
+                    res.status(404).send('User not found');
+                }
+            }
+        }
+    );
+});
+
+
 
 app.put('/users', (req, res) => {
     connection.query(
         'UPDATE `users` SET `fname`=?, `lname`=?, `username`=?, `password`=?, `phonenumber`=?, `avatar`=? WHERE id =?',
         [req.body.fname, req.body.lname, req.body.username, req.body.password, req.body.phonenumber, req.body.avatar, req.body.id],
-         function (err, results, fields) {
+        function (err, results, fields) {
             res.send(results)
         }
     )
@@ -62,21 +124,13 @@ app.delete('/users', (req, res) => {
     connection.query(
         'DELETE FROM `users` WHERE id =?',
         [req.body.id],
-         function (err, results, fields) {
+        function (err, results, fields) {
             res.send(results)
         }
     )
 })
 
-app.post('/login', function (req, res, next) {
-  connection.query(
-    'SELECT * FROM `users` WHERE username = ? and password = ?',
-    [req.body.username, req.body.password],
-    function(err, results) {
-      res.json(results);
-    }
-  );
-})
+
 
 /////////////////// ATTRACTION API //////////////////
 
